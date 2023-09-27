c5874e8c8c49d6a353847c95b3a63eec0ab23467
Rimoosoo <studentlimsoo@gmail.com>
2023-09-28 00:54:57 +0900

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ThemeProvider, createTheme } from "@mui/material";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+
+const theme = createTheme();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </ThemeProvider>
+  );
 }
