c20f9af5ce5f4c6fdfae9e6ef632535e60e6a6ff
SuJin <kimsujin001127@gmail.com>
2023-11-23 04:45:25 +0900

    fix: 기존으로 되돌리기

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var script = document.createElement('script');
-              script.src = '//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services&autoload=false';
-              document.body.appendChild(script);
-            `,
-          }}
-        />
+        <Script
+          strategy="beforeInteractive"
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services&autoload=false`}
+        ></Script>
         <Main />
         <NextScript />
       </body>
